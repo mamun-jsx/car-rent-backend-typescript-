ce05cfda4e4d78d737e5a7366f0d6cdecf17b5e7
almamun528 <ayapths@gmail.com>
2025-12-07 00:44:18 +0600

    setup route booking

--- src/modules/Bookings/booking.route.ts
+++ src/modules/Bookings/booking.route.ts
@@ -1,10 +1,10 @@
 import express from "express";
+import { bookingController } from "./booking.controller";
 
 const route = express.Router();
 
-route.post("/api/v1/bookings");
-route.get("/api/v1/bookings	");
-
-route.put("/api/v1/bookings/:bookingId");
+route.post("/api/v1/bookings", bookingController.createBooking);
+route.get("/api/v1/bookings", bookingController.getBooking);
+route.put("/api/v1/bookings/:bookingId", bookingController.updateBooking);
 
 export const bookingRoute = route;
--- src/server.ts
+++ src/server.ts
@@ -6,20 +6,25 @@ import { vehicleRoute } from "./modules/Vehicles/vehicles.route";
 dotenv.config();
 
 const app = express();
-const Port = process.env.port;
-app.use(express.json());
+const Port = process.env.port || 4001;
 
-initDB();
+//* =================================MIDDLEWARES=============================================================
+app.use(express.json());
+initDB(); // database initialization
+
 //* =================================ROUTES====================================================================
 app.use("/", userRoute); //*user route is register
-app.use('/',vehicleRoute) //*vehicle route is register
+app.use("/", vehicleRoute); //*vehicle route is register
+app.use("/", vehicleRoute); //*vehicle route is register
 //* ============================================================================================================
 
-//? Test route
+//* =================================ROOT ROUTE=============================================================
 app.get("/", async (req: Request, res: Response) => {
   res.status(200).json({ data: "Hit root route" });
 });
+//? ============================================================================================================
 
+//* =================================SERVER LISTENING=========================================================
 app.listen(Port, async () => {
   console.log(`Server is running on http://localhost:${Port}`);
 });
