09c65eae46c6a86eb7e0e61b4f68fc3cc6272f78
almamun528 <ayapths@gmail.com>
2025-12-06 23:13:57 +0600

    apiEndPoint file

---
+++ apiEndPoints.ts
@@ -0,0 +1,10 @@
+//____This file is used to define all API endpoints____//
+
+//* ================================== user API Endpoints ================================== //
+export const USER_API = {
+  CREATE_USER: "/api/v1/auth/signup", // POST
+  GET_USERS: "/api/v1/users", // GET
+  UPDATE_USER: "/api/v1/users/:userId", // PUT
+};
+
+//? ================================== vehicle API Endpoints ================================== //
